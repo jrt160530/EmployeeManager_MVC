8d52d81faf2c97d80de1b41257e00f647a3cd900
Joshua Thompson <jrt160530@outlook.com>
2020-09-27 13:39:26 -0500

    Used EF to add identity db context to sql server.
    dotnet ef migrations add IdentityMigration --context AppIdentityDbContext
    
    and
    
    dotnet ef database update --context AppIdentityDbContext

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using EmployeeManager_MVC.Security;
 
 namespace EmployeeManager_MVC
 {
@@ -32,8 +32,31 @@ namespace EmployeeManager_MVC
         {
             services.AddControllersWithViews();
 
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AppDbContext")));
+            // AddDbContext() method registers AppDbContext with the DI container
+            // Connection string passed to UseSqlServer
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("AppDb")));
 
+            // In this example, we are using the same db for the for identity as our above.
+            // Often they will be in different DB
+            services.AddDbContext<AppIdentityDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("AppDb")));
+
+            // AddIdentity() registers ASP.NET core Identity services with DI Container
+            // AddEntityFrameworkStores() adds an EF Core implementation of identity data stores
+            services.AddIdentity<AppIdentityUser, AppIdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>();
+
+            // By default ASp.NET Core Identity issues a cookie to an authenticed user.
+            // Below we've configured the Cookie for the login page's path.
+            // Any unauthorized attempts  (no cookie) redirected to login page.
+            // AccessDeniedPath sets display page path for cases of access attempts denied.
+            // For example a authenticated user attempting to access resources that user is not authorized to.
+            services.ConfigureApplicationCookie(opt =>
+           {
+               opt.LoginPath = "/Security/SignIn";
+               opt.AccessDeniedPath = "/Security/AccessDenied";
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +77,7 @@ namespace EmployeeManager_MVC
 
             app.UseRouting();
 
+            app.UseAuthorization();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
